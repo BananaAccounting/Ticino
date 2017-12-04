8c1e79e435bfd0100ed03cc0e8af2e65d0e0d371
Domenico Zucchetti <domenico@banana.ch>
2017-12-04 12:23:38 +0100

    Wordsearch segnala solo se trova tutte le parole

--- Patriziati/ElencoPatrizi/wordssearch.js
+++ Patriziati/ElencoPatrizi/wordssearch.js
@@ -1,4 +1,4 @@
-// Copyright [2015] [Banana.ch SA - Lugano Switzerland]
+// Copyright [2017] [Banana.ch SA - Lugano Switzerland]
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,12 +14,12 @@
 //
 // @id = ch.banana.app.search
 // @api = 1.0
-// @pubdate = 2017-06-06
+// @pubdate = 2017-12-04
 // @publisher = Banana.ch SA
-// @description.it = Cerca
-// @description.en = Search
-// @description.de = Suche
-// @description.fr = Recherche
+// @description.it = Cerca tutte le parole
+// @description.en = Search all words
+// @description.de = Alle Wörter durchsuchen
+// @description.fr = Rechercher tous les mots
 // @task = app.command
 // @doctype = *.*
 // @docproperties =
@@ -49,13 +49,13 @@ function exec() {
 	if (textToSearch) {
 		textToSearch = textToSearch.trim();
 		Banana.application.showMessages(); //Next messages are showed to the user through the message dialog
-		isFound = false;
+		var isFound = false;
 
 		//We create an array with the xml names of the tables in the document
 		var tablesArray = Banana.document.tableNames;
 
 		for (var i = 0; i < tablesArray.length; i++) {
-			searchText(tablesArray[i], textToSearch);
+			isFound |= searchText(tablesArray[i], textToSearch);
 		}
 
 		if (!isFound) {
@@ -73,36 +73,29 @@ function searchText(accouningTable, textToSearch) {
 	
 	var table = Banana.document.table(accouningTable);
 	var tableRow = Banana.document.table(accouningTable).rows;
+	var isFound = false;
 
 	for (var j = 0; j < tableRow.length; j++) {
 
 		if (!tableRow[j].isEmpty) {
 
-			var string = JSON.stringify(tableRow[j]);
-			var stringU = string.toUpperCase();
-			var stringL = string.toLowerCase();
-			var text = textToSearch.split(" ");
-
-			for (var i = 0; i < text.length; i++) {
-
-				if (string.indexOf(text[i]) > -1) { 
-					var tRow = tableRow[j].rowNr;
-					table.row(tRow).addMessage('"' + text[i] + '" ' + texts.found);
-					isFound = true;
-				}
-				else if (stringU.indexOf(text[i]) > -1) {
-					var tRow = tableRow[j].rowNr;
-					table.row(tRow).addMessage('"' + text[i] + '" ' + texts.found);
-					isFound = true;
-				}
-				else if (stringL.indexOf(text[i]) > -1) {
-					var tRow = tableRow[j].rowNr;
-					table.row(tRow).addMessage('"' + text[i] + '" ' + texts.found);
-					isFound = true;
+			var string = JSON.stringify(tableRow[j]).toLowerCase();
+			var words = textToSearch.toLowerCase().split(" ");
+			var isFoundRow = true;
+			/* search all words */
+			for (var i = 0; i < words.length; i++) {
+				if (string.indexOf(words[i]) == -1) { 
+					isFoundRow = false;
 				}
 			}
 		}
+		if (isFoundRow) {
+			var tRow = tableRow[j].rowNr;
+			table.row(tableRow[j].rowNr).addMessage(texts.found);
+			isFound = true;
+		}
 	}
+	return isFound;
 }
 
 
@@ -111,31 +104,31 @@ function setTexts(language) {
 	var texts = {};
 	if (language == 'enu') {
 		texts.dialogtxt1 = 'Find';
-		texts.dialogtxt2 = 'Search text';
+		texts.dialogtxt2 = 'Search words';
 		texts.dialogtxt3 = '';
-		texts.found = 'Text found';
-		texts.notFound = 'Text not found';
+		texts.found = 'Words found';
+		texts.notFound = 'Words not found';
 	}
 	else if (language == 'ita') {
 		texts.dialogtxt1 = 'Trova';
-		texts.dialogtxt2 = 'Cerca testo';
+		texts.dialogtxt2 = 'Cerca parole';
 		texts.dialogtxt3 = '';
-		texts.found = 'Testo trovato';
-		texts.notFound = 'Testo non trovato';
+		texts.found = 'Parole trovate';
+		texts.notFound = 'Parole non trovate';
 	}
 	else if (language == 'fra') {
 		texts.dialogtxt1 = 'Rechercher';
-		texts.dialogtxt2 = 'Chercher texte';
+		texts.dialogtxt2 = 'Mots à rechercher';
 		texts.dialogtxt3 = '';
-		texts.found = 'Texte trouvé';
-		texts.notFound = 'Texte non trouvé';
+		texts.found = 'Mots trouvés';
+		texts.notFound = 'Mots non trouvés';
 	}
 	else if (language == 'deu') {
 		texts.dialogtxt1 = 'Suchen';
-		texts.dialogtxt2 = 'Suchen nach';
+		texts.dialogtxt2 = 'Zu suchende Wörter';
 		texts.dialogtxt3 = '';
-		texts.found = 'Text gefunden';
-		texts.notFound = 'Text nicht gefunden';
+		texts.found = 'Wörter gefunden';
+		texts.notFound = 'Wörter nicht gefunden';
 	}
 	return texts;
 }
